748a756de1930e881ff6a7b07b888067c2e2fa01
bakharzy <1803773+bakharzy@users.noreply.github.com>
2024-02-22 17:33:58 +0200

    support service principal in management group scope

--- azure/scripts/service_principal/sp_create.sh
+++ azure/scripts/service_principal/sp_create.sh
@@ -3,37 +3,63 @@ GREEN=$'\e\033[0;32m'
 BLUE=$'\e\033[1;34m'
 NC=$'\e\033[0m' # No Color
 
-###################### Select default subscription ######################
-echo
-echo "${GREEN}Select your Azure Subscription OR press Enter to use the default ${NC}"
-echo  
-az account list -o table 
-echo
-read -p "Name or ID of the subscription: " subscription
-subscription=${subscription}
-if [ -z "$subscription" ]
-then
-        echo "${GREEN}No subscription entered, using default subscription${NC}"
-else
-        echo "${GREEN}Setting selected subscription ...${NC}"
-        az account set --subscription $subscription
-fi
-subscriptionID=$(az account show --query id -o tsv)
-# Verify the ID of the active subscription
-echo "${GREEN}Using subscription ID:${BLUE} $subscriptionID${NC}"
 
-###################### Creating Service Principal ######################
 echo
 read -p "Enter the Service Principal name you wish to create [${GREEN}Postfix '-sp' will be added${NC}]: " servicePrincipalName
 servicePrincipalName="${servicePrincipalName}-sp"
    
-read -p "Enter the role name to be assigned to service principal: " roleName
+read -p "Enter the Role Name to be assigned to service principal: " roleName
 roleName=${roleName}
 
-read -p "Enter the resource group name for role assignment scope: " resourceGroup
-resourceGroup=${resourceGroup}
 echo
+echo "Select the service principal scope ${BLUE}"  
+PS3='Select a scope: ' 
+echo
+options=("resource group" "management group" "quit")
+select opt in "${options[@]}"
 
-echo "${GREEN}Creating SP for RBAC with name $servicePrincipalName, with role $roleName and in scopes /subscriptions/$subscriptionID/resourceGroups/$resourceGroup${NC}"
-echo
-az ad sp create-for-rbac --name $servicePrincipalName --role $roleName --scopes /subscriptions/$subscriptionID/resourceGroups/$resourceGroup
+do
+    case $opt in
+        "management group")
+            read -p "Enter the Management Group name for role assignment scope: " managementGroup
+            managementGroup=${managementGroup}
+            echo
+            echo "${GREEN}Creating SP for RBAC with name ${BLUE}$servicePrincipalName${GREEN}, with role ${BLUE}$roleName${GREEN} and in scopes ${BLUE}/providers/Microsoft.Management/managementGroups/$managementGroup${NC}"
+            echo
+            az ad sp create-for-rbac --name $servicePrincipalName --role $roleName --scopes /providers/Microsoft.Management/managementGroups/$managementGroup
+            break
+            ;;
+        "resource group")
+            ###################### Select default subscription ######################
+            echo
+            echo "${GREEN}Select your Azure Subscription OR press Enter to use the default ${NC}"
+            echo  
+            az account list -o table 
+            echo
+            read -p "Name or ID of the subscription: " subscription
+            subscription=${subscription}
+            if [ -z "$subscription" ]
+            then
+                echo "${GREEN}No subscription entered, using default subscription${NC}"
+            else
+                echo "${GREEN}Setting selected subscription ...${NC}"
+                az account set --subscription $subscription
+            fi
+            subscriptionID=$(az account show --query id -o tsv)
+            # Verify the ID of the active subscription
+            echo "${GREEN}Using subscription ID:${BLUE} $subscriptionID${NC}"
+
+            read -p "Enter the Resource Group name for role assignment scope: " resourceGroup
+            resourceGroup=${resourceGroup}
+            echo
+            echo "${GREEN}Creating SP for RBAC with name ${BLUE}$servicePrincipalName${GREEN}, with role ${BLUE}$roleName${GREEN} and in scopes ${BLUE}/subscriptions/$subscriptionID/resourceGroups/$resourceGroup${NC}"
+            echo
+            az ad sp create-for-rbac --name $servicePrincipalName --role $roleName --scopes /subscriptions/$subscriptionID/resourceGroups/$resourceGroup
+            break
+            ;;
+        "quit")
+            break
+            ;;
+        *) echo "invalid option";;
+    esac
+done
